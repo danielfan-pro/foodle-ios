03c5e8ea796195ee4ea46bab7cfc11195098931e
Daniel Fan <xuhuafan@gmail.com>
2026-04-02 13:02:10 -0400

    Initial Commit

---
+++ foodle/foodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodleApp.swift
+//  foodle
+//
+//  Created by Daniel Fan on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct foodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodleTests/foodleTests.swift
@@ -0,0 +1,18 @@
+//
+//  foodleTests.swift
+//  foodleTests
+//
+//  Created by Daniel Fan on 4/2/26.
+//
+
+import Testing
+
+struct foodleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
